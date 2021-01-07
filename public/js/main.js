9a6a7370ff986277a4cd23550240115755b7fd4b
Alex <agasparr98@gmail.com>
2021-01-07 12:09:23 +0100

    exo fini

---
+++ public/js/main.js
@@ -0,0 +1,48 @@
+// - # Exo 1 
+//   - ## Créer une variable qui recoit un jour de la semaine via un prompt.
+//   - ## A l'aide d'un switch, affichez ce que vous devez faire pendant le jour que vous avez choisi:
+
+//     - ### Ex: On est lundi. Tu as cours a MG et en soirée tu as sport.
+
+// let jour = prompt("jour?");
+// switch (jour) {
+//     case "lundi":
+//         console.log("je vais en cours je mange je dors");
+//         break;
+//     case "mardi":
+//         console.log("je vais en cours je mange je dors");
+//         break;
+//     case "mercredi":
+//         console.log("je vais en cours je mange je dors");
+//         break;   
+//     case "jeudi":
+//         console.log("je vais en cours je mange je dors");
+//         break;
+//     case "vendredi":
+//         console.log("je vais en cours je mange je dors");
+//         break; 
+//     case "samedi":
+//         console.log("je fais la fête car c'est le weekend, wahou");
+//         break; 
+//     case "dimanche":
+//         console.log("je fais la fête mais pas trop tard parceque attention demain c'est lundi");
+//         break; 
+//     default:
+//         console.log("tu ne m'as pas donné un jour de la semaine, zebi");
+//         break;
+// }
+
+// - # Exo 2 
+//   - ## Créer une variable qui recoit ton age via un prompt.
+//   - ## A l'aide d'un switch, affichez si vous avez plus ou moins de 18 ans.
+
+// let age = parseInt(prompt("age?"));
+
+// switch (true) {
+//     case (age >= 18):
+//         alert("t majeur");
+//         break;
+//     default:
+//         console.log("t mineur");
+//         break;
+// }
